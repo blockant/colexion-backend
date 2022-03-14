7e05ddad977235bcb458c0ecce8c720b49102d8b
pcsingh <premchandra.singh.5268@gmail.com>
2022-03-15 02:44:20 +0530

    POST request for getting the user by ID

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -6,131 +6,131 @@ import Locals from "../providers/Locals";
 import ErrorHandler from "../providers/Error";
 import Follows from "../models/Follower";
 // import processFileMiddleware from "../middlewares/Upload";
-class Users{
+class Users {
     /**
      * @param {req, res} 
      * @returns Edited User
      * @desc Edits a logged in user based on ID
      */
-    public static async editUser(req: Request, res: Response){
+    public static async editUser(req: Request, res: Response) {
         try {
             console.log('Request Body is', req.body)
-            const {name,password, bio, external_urls}=req.body
+            const { name, password, bio, external_urls } = req.body
             // await processFileMiddleware(req, res);
             console.log("File is ", req.files)
-            let foundUser= await User.findById(res.locals.userId)
-            if(!foundUser){
+            let foundUser = await User.findById(res.locals.userId)
+            if (!foundUser) {
                 throw new Error('User Not Found')
             }
-            if(name){
-                foundUser.name=name
+            if (name) {
+                foundUser.name = name
             }
-            if(password){
-                foundUser.password=password
+            if (password) {
+                foundUser.password = password
             }
-            if(bio){
-                foundUser.bio=bio
+            if (bio) {
+                foundUser.bio = bio
             }
-            if(external_urls){
-                foundUser.external_urls=external_urls
+            if (external_urls) {
+                foundUser.external_urls = external_urls
             }
-            if(Array.isArray(req.files)){
+            if (Array.isArray(req.files)) {
                 for (const file of req.files) {
-                    if(file.fieldname==='profile_pic'){
-                        const image_url=await AWSService.uploadToS3Bucket(file, Locals.config().awsS3Bucket)
-                        foundUser.avatar=image_url
+                    if (file.fieldname === 'profile_pic') {
+                        const image_url = await AWSService.uploadToS3Bucket(file, Locals.config().awsS3Bucket)
+                        foundUser.avatar = image_url
                     }
-                    if(file.fieldname==='profile_banner'){
-                        const image_url=await AWSService.uploadToS3Bucket(file, Locals.config().awsS3Bucket)
-                        foundUser.banner=image_url
+                    if (file.fieldname === 'profile_banner') {
+                        const image_url = await AWSService.uploadToS3Bucket(file, Locals.config().awsS3Bucket)
+                        foundUser.banner = image_url
                     }
                 }
             }
             await foundUser.save()
-            foundUser= await User.findById(res.locals.userId).select('-password')
-            return res.status(200).json({message: 'User Updated Success', user: foundUser})
+            foundUser = await User.findById(res.locals.userId).select('-password')
+            return res.status(200).json({ message: 'User Updated Success', user: foundUser })
         } catch (err) {
             console.log(err)
             Logger.error(err)
-            if(err instanceof Error){
-                return res.status(500).json({message: 'Server Error', error: err.message})
-            }else{
-                return res.status(500).json({message: 'Server Error of Unhandledd Type'})
+            if (err instanceof Error) {
+                return res.status(500).json({ message: 'Server Error', error: err.message })
+            } else {
+                return res.status(500).json({ message: 'Server Error of Unhandledd Type' })
             }
         }
     }
-    public static async getUserById(req: Request, res: Response){
-        try{
-            const {userId}=req.params
-            const foundUser= await User.findById(userId).select('-password')
-            if(!foundUser){
+    public static async getUserById(req: Request, res: Response) {
+        try {
+            const { userId } = req.body
+            const foundUser = await User.findById(userId).select('-password')
+            if (!foundUser) {
                 throw new Error('User Not Found')
             }
-            return res.status(200).json({message: 'Found User Is', user: foundUser})
-        }catch(err){
+            return res.status(200).json({ message: 'Found User Is', user: foundUser })
+        } catch (err) {
             return ErrorHandler.APIErrorHandler(err, res)
         }
     }
-    public static async getLoggedInUser(req: Request, res: Response){
-        try{
-            const foundUser=await User.findById(res.locals.userId).select('-password')
-            return res.status(200).json({mesage: 'Succes', user: foundUser})
-        }catch(err){
+    public static async getLoggedInUser(req: Request, res: Response) {
+        try {
+            const foundUser = await User.findById(res.locals.userId).select('-password')
+            return res.status(200).json({ mesage: 'Succes', user: foundUser })
+        } catch (err) {
             return ErrorHandler.APIErrorHandler(err, res)
         }
     }
-    public static async addWallet(req: Request, res: Response){
+    public static async addWallet(req: Request, res: Response) {
 
     }
-    public static async toggleFollowUser(req: Request, res: Response){
-        try{
-            const {following_id, operation}=req.body
-            if(operation==='FOLLOW'){
-                const foundFollow=await Follows.findOne({follower: res.locals.userId, following: following_id})
-                if(foundFollow){
+    public static async toggleFollowUser(req: Request, res: Response) {
+        try {
+            const { following_id, operation } = req.body
+            if (operation === 'FOLLOW') {
+                const foundFollow = await Follows.findOne({ follower: res.locals.userId, following: following_id })
+                if (foundFollow) {
                     throw new Error('User already follows this user')
-                }else{
-                    await Follows.create({follower: res.locals.userId, following: following_id})
-                    return res.status(200).json({message: 'Success, followed Success'})
+                } else {
+                    await Follows.create({ follower: res.locals.userId, following: following_id })
+                    return res.status(200).json({ message: 'Success, followed Success' })
                 }
-            }else if(operation==='UNFOLLOW'){
-                const foundFollow=await Follows.findOne({follower: res.locals.userId, following: following_id})
-                if(!foundFollow){
+            } else if (operation === 'UNFOLLOW') {
+                const foundFollow = await Follows.findOne({ follower: res.locals.userId, following: following_id })
+                if (!foundFollow) {
                     throw new Error('User does not follows this user')
-                }else{
-                    await Follows.deleteOne({_id: foundFollow._id})
-                    return res.status(200).json({message: 'Success, unfollowed Success'})
+                } else {
+                    await Follows.deleteOne({ _id: foundFollow._id })
+                    return res.status(200).json({ message: 'Success, unfollowed Success' })
                 }
-            }else{
+            } else {
                 throw new Error('Illegal Operation')
             }
-        }catch(err){
+        } catch (err) {
             return ErrorHandler.APIErrorHandler(err, res)
         }
     }
-    public static async getFollowingInfo(req: Request, res: Response){
-        try{
-            const {following_id}=req.query
-            const foundFollow=await Follows.findOne({follower: res.locals.userId, following: following_id})
-            if(!foundFollow){
-                return res.status(404).json({message: 'No follow Exist'})
-            }else{
-                return res.status(200).json({message: 'Follow exists'})
+    public static async getFollowingInfo(req: Request, res: Response) {
+        try {
+            const { following_id } = req.query
+            const foundFollow = await Follows.findOne({ follower: res.locals.userId, following: following_id })
+            if (!foundFollow) {
+                return res.status(404).json({ message: 'No follow Exist' })
+            } else {
+                return res.status(200).json({ message: 'Follow exists' })
             }
-        }catch(err){
+        } catch (err) {
             return ErrorHandler.APIErrorHandler(err, res)
         }
     }
-    public static async getAllUsers(req: Request, res: Response){
-        try{
-            const options={
+    public static async getAllUsers(req: Request, res: Response) {
+        try {
+            const options = {
                 page: Number(req.query.page) || 1,
                 limit: Number(req.query.limit) || 10,
                 select: '-password'
             }
-            const foundUsers=await User.paginate({}, options)
-            return res.status(200).json({message: 'Users Found Success', foundUsers})
-        }catch(err){
+            const foundUsers = await User.paginate({}, options)
+            return res.status(200).json({ message: 'Users Found Success', foundUsers })
+        } catch (err) {
             return ErrorHandler.APIErrorHandler(err, res)
         }
     }
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -2,20 +2,23 @@ import { Router } from "express";
 import { isLoggedIn } from "../middlewares/Auth";
 import Users from "../controllers/User";
 import upload from "../middlewares/Upload";
-const router=Router()
+const router = Router()
 
 // Get Logged in User
-router.get('/',[isLoggedIn], Users.getLoggedInUser)
+router.get('/', [isLoggedIn], Users.getLoggedInUser)
+
+// Get User Info by id
+router.post('/id', Users.getUserById)
 
 // Edit User Info
-router.put('/',[isLoggedIn, upload.any()], Users.editUser)
+router.put('/', [isLoggedIn, upload.any()], Users.editUser)
 
 // Get all Users
 router.get('/all', Users.getAllUsers)
 
 //Follow Unfollow a User
-router.post('/follow',[isLoggedIn], Users.toggleFollowUser)
+router.post('/follow', [isLoggedIn], Users.toggleFollowUser)
 
 //Get Following Info
-router.get('/follow',[isLoggedIn],Users.getFollowingInfo)
+router.get('/follow', [isLoggedIn], Users.getFollowingInfo)
 export default router
