7b9e3d6a0a2515d82831654063c7b4dab5375320
pcsingh <premchandra.singh.5268@gmail.com>
2022-03-16 00:10:17 +0530

    CastError Resolved

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -2,23 +2,24 @@ import { Router } from "express";
 import { isLoggedIn } from "../middlewares/Auth";
 import Users from "../controllers/User";
 import upload from "../middlewares/Upload";
-const router=Router()
+const router = Router()
 
 // Get Logged in User
-router.get('/',[isLoggedIn], Users.getLoggedInUser)
-
-//Get User By Id
-router.get('/:userId', Users.getUserById)
+router.get('/', [isLoggedIn], Users.getLoggedInUser)
 
 // Edit User Info
-router.put('/',[isLoggedIn, upload.any()], Users.editUser)
+router.put('/', [isLoggedIn, upload.any()], Users.editUser)
 
 // Get all Users
 router.get('/all', Users.getAllUsers)
 
 //Follow Unfollow a User
-router.post('/follow',[isLoggedIn], Users.toggleFollowUser)
+router.post('/follow', [isLoggedIn], Users.toggleFollowUser)
 
 //Get Following Info
-router.get('/follow',[isLoggedIn],Users.getFollowingInfo)
+router.get('/follow', [isLoggedIn], Users.getFollowingInfo)
+
+//Get User By Id
+router.get('/:userId', Users.getUserById)
+
 export default router
